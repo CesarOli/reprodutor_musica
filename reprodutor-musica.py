df99077d043e0a96a1490570c5d736836cb5bca3
CesarOli <cesaraleoliveira@gmail.com>
2023-07-09 22:43:44 -0300

    Configuração do banco de dados reprodutor-musica.

--- reprodutor-musica.py
+++ reprodutor-musica.py
@@ -2,3 +2,9 @@ import mysql.connector
 import requests
 from time import sleep
 
+config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'SENHA_DESEJADA',
+    'database': 'reprodutor-musica'
+}
