9884dde4ff391395fd8b2aea61d97cd83c643af1
CesarOli <cesaraleoliveira@gmail.com>
2023-07-10 23:16:13 -0300

    Criação  menu interativo com usuário.

--- reprodutor-musica.py
+++ reprodutor-musica.py
@@ -8,3 +8,10 @@ config = {
     'password': 'SENHA_DESEJADA',
     'database': 'reprodutor-musica'
 }
+
+while True:
+    print("========== Reprodutor de Música ==========")
+    print("1. Adicionar Música")
+    print("2. Pesquisar Música")
+    print("3. Listar Todas as Músicas")
+    print("8. Sair")
