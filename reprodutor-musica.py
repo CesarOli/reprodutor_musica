370d45040f890e5d766dfd16f953678ff175b375
CesarOli <cesaraleoliveira@gmail.com>
2023-07-11 23:27:15 -0300

    Adição função sleep, buscando melhor interação com usuário.

--- reprodutor-musica.py
+++ reprodutor-musica.py
@@ -11,9 +11,15 @@ config = {
 
 while True:
     print("========== Reprodutor de Música ==========")
+    sleep(0.8)
+    print("Aguarde, abrindo menu de opções")
+    sleep(1.5)
     print("1. Adicionar Música")
+    sleep(0.3)
     print("2. Pesquisar Música")
+    sleep(0.3)
     print("3. Listar Todas as Músicas")
+    sleep(0.3)
     print("0. Sair")
 
     opcao = input("Digite o número da opção desejada: ")
@@ -27,9 +33,15 @@ while True:
         escolha = input("Voltar para o menu? (S/N): ")
         if escolha.lower() != "s":
             break
+
+    elif opcao == "0":
+        break
     else:
         print("Opção inválida. Digite novamente.")
-
+print("Encerrando o programa...")
+sleep(1)
+print("Obrigado!!")
+sleep(1.5)
 print("Fim do programa.")
 
 
